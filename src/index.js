ec693ddda433c2e4882293e3a83b21e1ab4944b5
Kurnosov <dryush97@gmail.com>
2020-10-01 21:45:43 +0300

    feat: min, max, avg implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
-exports.min = function min (array) {
-  return 0;
+/**
+ * @param {Array} array 
+ */
+exports.min = function min(array) {
+  let minimal = 0;
+  if (array && array.length) {
+    minimal = array[0];
+    for (let i = 1; i + 1 <= array.length; i++) {
+      if (array[i] < minimal) {
+        minimal = array[i];
+      }
+    }
+  }
+  return minimal;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let maxim = 0;
+  if (array && array.length) {
+    maxim = array[0];
+    for (let i = 1; i + 1 <= array.length; i++) {
+      if (array[i] > maxim) {
+        maxim = array[i];
+      }
+    }
+  }
+  return maxim;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let avgValue = 0;
+  if (array && array.length) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    avgValue = sum / array.length;
+  }
+  return avgValue;
 }
